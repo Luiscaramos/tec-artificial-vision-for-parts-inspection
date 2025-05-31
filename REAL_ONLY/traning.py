211d526e61be64878d11f4186c7a4277f07be968
luiscaramos <luiscar.ramzaz@gmail.com>
2025-05-31 06:30:18 -0600

    REAL ONLY DATA

---
+++ REAL_ONLY/traning.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Cargar un modelo preentrenado o uno nuevo
+model = YOLO('yolov8m.pt')  # Cambia a yolov8s.pt, yolov8m.pt, etc., según tu necesidad
+
+# Entrenar
+if __name__ == "__main__":
+    model.train(data='C:\Users\Luis Carlos Ramos\Personal\tec-artificial-vision-for-parts-inspection\REAL_ONLY\data.yaml', epochs=50, imgsz=640, device=0)  # Usa device=0 para CUDA
