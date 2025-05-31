b9853e611666fcd38511ac059c3bdadb24c88917
luiscaramos <luiscar.ramzaz@gmail.com>
2025-05-31 07:13:55 -0600

    test y resultados de 3 modelos

--- REAL_ONLY/traning.py
+++ REAL_ONLY/traning.py
@@ -5,4 +5,4 @@ model = YOLO('yolov8m.pt')  # Cambia a yolov8s.pt, yolov8m.pt, etc., según tu n
 
 # Entrenar
 if __name__ == "__main__":
-    model.train(data='C:\Users\Luis Carlos Ramos\Personal\tec-artificial-vision-for-parts-inspection\REAL_ONLY\data.yaml', epochs=50, imgsz=640, device=0)  # Usa device=0 para CUDA
+    model.train(data=r'C:\Users\Luis Carlos Ramos\Personal\tec-artificial-vision-for-parts-inspection\REAL_ONLY\data.yaml', epochs=50, imgsz=640, device=0)  # Usa device=0 para CUDA
